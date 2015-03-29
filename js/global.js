bb9edb75288a729d16d9ff9c2b0e78cd1fc12468
Eduardo Offermann Palma <eofferma@gmail.com>
2015-03-28 23:26:58 -0300

    Nueva version de página luzyvida.cl

---
+++ js/global.js
@@ -0,0 +1,121 @@
+jQuery(document).ready(function ($) {
+    "use strict";
+    
+    //MENU
+    $('.to_menu').on('click', function (e) {
+        e.preventDefault();
+        
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+            $(this).next('nav').hide();
+        } else {
+            $(this).addClass('active');
+            $(this).next('nav').show();
+        }
+    });
+    
+    //SELECT TERAPIAS  
+    $('.select_area .arrow_select, .select_area span').click(function () {
+        if ($('body').width() < 751) {
+            if ($(this).parent('.select_area').find('.therapy_sel').is(':visible')) {
+                $(this).parent('.select_area').find('.therapy_sel').slideUp(200);
+            } else {
+                $('.select_area .therapy_sel').hide();
+                $(this).parent('.select_area').find('.therapy_sel').slideDown(200);
+            }
+        }
+    });
+    $('.select_area li').click(function () {
+        if ($('body').width() < 751) {
+            var valSelect = $(this).children('a').html();
+            $(this).parents('.select_area').children('input').val(valSelect);
+            $(this).parents('.select_area').children('span').hide().html(valSelect).fadeIn(300);
+            $(this).parent('.therapy_sel').slideUp(200);
+        }
+    });
+
+    //BANNER
+    $('#banner .slider').bxSlider({
+        mode: 'fade',
+        pager: false,
+        autostart: true,
+        prevText: '',
+        nextText: ''
+    });
+    //TERAPIAS
+    $('#terapias .content_area').bxSlider({
+        pagerCustom: '#the_sel',
+        controls: false,
+        mode: 'fade'
+    });
+    $('#terapias .content_pic').bxSlider({
+        pagerCustom: '#the_sel',
+        controls: false,
+        mode: 'fade'
+    });
+    
+    //TERAPEUTAS
+    $('#terapeutas .team').jcarousel({
+        easing: 'linear',
+        wrap: 'circular',
+        animation: 2000
+    })
+    $('#terapeutas .jcarousel-prev').jcarouselControl({
+        target: '-=1'
+    });
+
+    $('#terapeutas .jcarousel-next').jcarouselControl({
+        target: '+=1'
+    });
+    
+    $('#spec_the a').on('click', function (e) {
+        e.preventDefault();
+    });
+    
+    $(document).on('scroll', onScroll);
+    
+    //SCROLL MENU
+    $('nav li a, .scrolling, h1 a').click(function (e) {
+        var target = $(this).attr("href");
+        goToSection(target, e);
+        $(this).parent().parent().parent().removeClass('active');
+    });
+    
+    //WIDGET COMENTARIOS
+    $('#opiniones .comments').bxSlider({
+        mode: 'fade',
+        pagerCustom: '.ctrl_comments',
+        controls: false
+    });
+    
+    
+    //WIDTH
+    if ($(window).width < 769) {
+        $('body').addClass('mobile');
+    } else {
+        $('body').removeClass('mobile');
+    }
+    
+});
+//FUNCIONAMIENTO SCROLL MENU
+var goToSection = function (idSection, event) {
+    "use strict";
+    event.preventDefault();
+    $('body, html').stop().animate({scrollTop: $(idSection).offset().top - 80}, 800);
+};
+
+var onScroll = function (event) {
+    "use strict";
+    var scrollPos = $(document).scrollTop();
+    
+    $('header nav li a').each(function () {
+        var currLink = $(this),
+            refElement = $(currLink.attr('href'));
+        if (refElement.position().top - 80 <= scrollPos && refElement.position().top - 80 + refElement.height() > scrollPos) {
+            $('header nav li').parent('li').removeClass('active');
+            $(this).parent('li').addClass('active');
+        } else {
+            currLink.parent('li').removeClass('active');
+        }
+    });
+};
---
+++ js/maps.js
@@ -0,0 +1,35 @@
+/*jslint browser: true, devel: true*/
+/*global $, jQuery, alert*/
+
+//// GOOGLE MAPS
+
+function initialize() {
+    "use strict";
+    var mapOptions = {
+        zoom: 16,
+        center: new google.maps.LatLng(-33.439431, -70.607278),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        
+        scrollwheel: false,
+        zoomControl: true,
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.DEFAULT
+        },
+        overviewMapControl: true
+    },
+        map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions),
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(-33.439431, -70.607278),
+            map: map,
+            title: 'Luz y Vida'
+        });
+    
+    //MANTENER CENTRO
+    google.maps.event.addDomListener(window, 'resize', function() {
+        var center = map.getCenter();
+        google.maps.event.trigger(map, 'resize');
+        map.setCenter(center);
+    });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
